41743b9f9c8026721bf4ff77412a939b5e7b49d5
RexGLiu <49000792+RexGLiu@users.noreply.github.com>
2022-11-13 21:42:17 -0500

    Agent now samples entire task datasets.
    Replaced replay buffer with task buffer.
    
    Entire task datasets stored in buffer.
    
    Training loop samples entire datasets rather than minibatches of data samples.

--- SAC.py
+++ SAC.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .ReplayBuffer import ReplayBuffer
+from .TaskBuffer import TaskBuffer as Buffer
 from .model import TrainNet, Critic, Value
 
 import torch
@@ -7,9 +7,9 @@ from torch.optim import Adam
 
 
 class SAC:
-    def __init__(self, env, actor, buffer_size=1E6, n_train_steps=2E7, train_start=5E3,
-                 latent_burn_in=40, ep_min=100, ep_max=1000, sample_thresh=0.5,
-                 lr=3E-4, smoothing_coeff=0.005, optimizer=Adam):
+    def __init__(self, env, actor, n_buffer_tasks=1E4, n_train_steps=2E7, train_start=5E3,
+                 set_min=100, set_max=1000, batch_size = 2, latent_burn_in=True, burn_in_frac=0.5, sample_thresh=0.5,
+                 lr=3E-4, smoothing_coeff=0.005, optimizer=Adam, data_net_hidden = 8):
         self.env = env
 
         self.act_dim = env.n_actions
@@ -20,7 +20,6 @@ class SAC:
         self.train_start = int(train_start)
         self.n_train_steps = int(n_train_steps)
 
-        data_net_hidden = 8
         self.data_net = TrainNet(self.input_dim, self.output_dim, data_net_hidden)
 
         self.actor = actor
@@ -29,17 +28,16 @@ class SAC:
         self.value = Value(actor.hidden_size)
         self.target_value = Value(actor.hidden_size)
 
-        self.buffer_size = int(buffer_size)
-        self.buffer = ReplayBuffer(self.buffer_size, self.obs_dim, actor.hidden_size, self.act_dim)
+        self.n_buffer_tasks = int(n_buffer_tasks)
+        self.buffer = Buffer(self.n_buffer_tasks, self.set_max, self.input_dim, self.output_dim)
         self.latent_burn_in = latent_burn_in
+        self.burn_in_frac = burn_in_frac
 
         self.sample_thresh = sample_thresh
 
-        self.ep_min = ep_min
-        self.ep_max = ep_max
-        self.ep_len = 0
-        self.train_x = None
-        self.train_y = None
+        self.set_min = set_min
+        self.set_max = set_max
+        self.batch_size = batch_size
 
         self.lr = lr
         self.smoothing_coeff = smoothing_coeff
@@ -50,96 +48,81 @@ class SAC:
 
 
     def train(self):
-        env_step = 0
-        for step in range(self.train_start):
-            if env_step == 0:
-                # randomize target network and generate new training data
-                self._generate_training_data()
-                self.env.reset()
 
-                # sample an initial, random action
-                # note: first step uses dummy inputs to LSTM and will not be stored in replay buffer
-                obs = np.zeros(self.obs_dim + self.act_dim + 1)
+        # initial steps will only populate task buffer with training sets
+        for step in range(self.train_start):
+            train_x, train_y, set_size = self._generate_training_data()
+            self.buffer.add(train_x, train_y, set_size)
+
+        # training loop
+        for step in range(self.train_start + 1, self.n_train_steps):
+            train_x, train_y, set_size = self._generate_training_data()
+            self.buffer.add(train_x, train_y, set_size)
+
+            # retrieve training set to train on
+            x_batch, y_batch, batch_set_sizes = self.buffer.sample(self.batch_size)
+
+            # train on retrieved batches
+            for ii in range(self.batch_size):
+                set_size = batch_set_sizes[ii]
+                train_x = x_batch[ii, :set_size]
+                train_y = y_batch[ii, :set_size]
+
+                idx_permutation = np.random.permutation(set_size)
+                train_x = train_x[idx_permutation]
+                train_y = train_y[idx_permutation]
+
+                n_burn_in_steps = 0
+                if self.latent_burn_in:
+                    n_burn_in_steps = round(set_size * burn_in_frac)
+                    n_burn_in_steps = np.random.choice(n_burn_in_steps)
+
+                # first action is arbitrary
+                input = np.zeros(self.obs_dim + self.act_dim + 1)
                 hidden = None
                 cell = None
 
-            _, (action_p, hidden, cell) = self.actor.forward(obs, hidden, cell)
-            action = self.actor.sample(p=action_p, thresh=self.sample_thresh)
+                # burn in steps to initialize actor hidden state
+                for jj in range(n_burn_in_steps):
+                    _, (action_p, hidden, cell) = self.actor.forward(input, hidden, cell)
+                    action = self.actor.sample(p=action_p, thresh=self.sample_thresh)
 
-            x = self.train_x[env_step]
-            y_target = self.train_y[env_step]
-            y, loss = self.env.step((x, y_target, action))
-            y = y.numpy()
-            loss = loss.numpy()
+                    x = train_x[jj]
+                    y_target = train_y[jj]
+                    y, loss = self.env.step((x, y_target, action))
+                    y = y.numpy()
+                    loss = loss.numpy()
 
-            obs = np.concatenate((x, y, y_target), axis=1)
+                    input = np.concatenate((x, y, y_target, loss, action), axis=1)
 
-            # change environment and target func controller must optimize
-            env_step += 1
-            done = (env_step == self.ep_len)
-            if done:
-                env_step = 0
+                # training steps
+                for jj in range(n_burn_in_steps, set_size):
+                    _, (action_p, hidden, cell) = self.actor.forward(input, hidden, cell)
+                    action = self.actor.sample(p=action_p, thresh=self.sample_thresh)
 
-            self.buffer.add(obs, hidden, cell, action, loss, done)
+                    x = train_x[jj]
+                    y_target = train_y[jj]
+                    y, loss = self.env.step((x, y_target, action))
+                    y = y.numpy()
+                    loss = loss.numpy()
 
-        for step in range(self.train_start+1, self.n_train_steps):
-            if env_step == 0:
-                # randomize target network and generate new training data
-                self._generate_training_data()
-                self.env.reset()
+                    input = np.concatenate((x, y, y_target, loss, action), axis=1)
 
-                # sample an initial, random action
-                # note: first step uses dummy inputs to LSTM and will not be stored in replay buffer
-                obs = np.zeros(self.obs_dim + self.act_dim + 1)
-                hidden = np.zeros(self.actor.hidden_size)
-                cell = np.zeros(self.actor.hidden_size)
+                    obs = torch.from_numpy(obs)
+                    hidden = torch.from_numpy(hidden)
+                    cell = torch.from_numpy(cell)
+                    action = torch.from_numpy(action)
+                    loss = torch.from_numpy(loss)
 
-            _, (action_p, hidden2, cell2) = self.actor.forward(obs, hidden, cell)
-            action = self.actor.sample(p=action_p, thresh=self.sample_thresh)
 
-            # get "training data" for target network
-            x = self.train_x[env_step]
-            y_target = self.train_y[env_step]
-            y, loss = self.env.step((x, y_target, action))
-            y = y.numpy()
-            loss = loss.numpy()
-
-            obs = np.concatenate((x, y, y_target), axis=1)
-
-            # if training set complete, change environment and target func controller must optimize
-            env_step += 1
-            done = (env_step == self.ep_len)
-            if done:
-                env_step = 0
-
-            self.buffer.add(obs, hidden, cell, action, loss, done)
-            hidden = hidden2
-            cell = cell2
-
-            self._grad_update()
-
-    def _grad_update(self):
-        # Compute gradients
-        obs, hidden, cell, action, rew, done = self.buffer.sample()
-        obs = torch.from_numpy(obs)
-        hidden = torch.from_numpy(hidden)
-        cell = torch.from_numpy(cell)
-        action = torch.from_numpy(action)
-        rew = torch.from_numpy(rew)
-
-        # burn in of LSTM hidden states
-        for ii in range(self.latent_burn_in):
-            _, (_, hidden, cell) = self.actor.forward(obs, hidden, cell)
-
-        # asd
 
     def _evaluate(self):
         pass
 
     def _generate_training_data(self):
-        self.ep_len = np.random.randint(self.ep_min, self.ep_max + 1)
-        self.train_x = np.zeros(self.ep_len, self.input_dim, dtype=np.float32)
-        self.train_y = np.zeros(self.ep_len, self.output_dim, dtype=np.float32)
+        set_size = np.random.randint(self.set_min, self.set_max + 1)
+        train_x = np.zeros(set_size, self.input_dim, dtype=np.float32)
+        train_y = np.zeros(set_size, self.output_dim, dtype=np.float32)
 
         # set noise level for output
         max_std = 3
@@ -151,7 +134,7 @@ class SAC:
 
         x_std = 10
         with torch.inference_mode():
-            for ii in range(self.ep_len):
+            for ii in range(set_size):
                 x_sample = x_std*torch.randn(size=(1, self.input_dim))
                 y_sample = self.data_net(x_sample)
 
@@ -160,5 +143,7 @@ class SAC:
                 y_sample += y_noise
 
                 # store dataset
-                self.train_x[ii] = x_sample.numpy()
-                self.train_y[ii] = y_sample.numpy()
+                train_x[ii] = x_sample.numpy()
+                train_y[ii] = y_sample.numpy()
+
+        return train_x, train_y, set_size
